d77cf34d7836982aa71018762d24e29a327e595b
BustyBasti <sebastian.k.schmidt@gmx.de>
2015-04-30 21:22:08 +0200

    Changing from OOP in Excercise 2

--- Aufgabe 2/Numerical Derivation.py
+++ Aufgabe 2/Numerical Derivation.py
@@ -1,6 +1,12 @@
-from __future__ import division
-__author__ = 'Sebastian'
+"""
+Aufgabe 2.1
 
+Dieses Programm dient dem Kennenlernen elementarer numerischer
+Differentiationsmethoden und dem Illustrieren der Fehlerquellen ebenjener
+Methoden in Abhaengigkeit vom Differentiationsparameter h.
+"""
+
+from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,22 +19,18 @@ def fprime(x):
     return 2*x/(x**4 + 1)
 
 
-class Function(object):
-    def __init__(self, function):
-        self.defn = function
-
-    def forward(self, x_0, h):
-        return (self.defn(x_0 + h) - self.defn(x_0))/h
-
-    def central(self, x_0, h):
-        return (self.defn(x_0 + h/2) - self.defn(x_0 - h/2))/h
-
-    def extrapolate(self, x_0, h):
-        return (8 * (self.defn(x_0 + h/4) - self.defn(x_0 - h/4)) -
-               (self.defn(x_0 + h/2) - self.defn(x_0 - h/2)))/(3 * h)
+def forward(func, x_0, h):
+    return (func(x_0 + h) - func(x_0))/h
 
 
-F = Function(f)
+def central(func, x_0, h):
+    return (func(x_0 + h/2) - func(x_0 - h/2))/h
+
+
+def extrapolate(func, x_0, h):
+    return (8 * (func(x_0 + h/4) - func(x_0 - h/4)) -
+            (func(x_0 + h/2) - func(x_0 - h/2)))/(3 * h)
+
 
 h_list = 10**(np.linspace(-10.0, 0.0, 1001))
 error_forward = []
@@ -36,15 +38,21 @@ error_central = []
 error_extrapolate = []
 
 for z in h_list:
-    error_forward.append(abs((fprime(1/3) - F.forward(1/3, z))/fprime(1/3)))
-    error_central.append(abs((fprime(1/3) - F.central(1/3, z))/fprime(1/3)))
-    error_extrapolate.append(abs((fprime(1/3) - F.extrapolate(1/3, z))/fprime(1/3)))
+    error_forward.append(abs((fprime(1/3) - forward(f, 1/3, z))/fprime(1/3)))
+    error_central.append(abs((fprime(1/3) - central(f, 1/3, z))/fprime(1/3)))
+    error_extrapolate.append(abs((fprime(1/3) - extrapolate(f, 1/3, z))/fprime(1/3)))
 
-plt.subplot(111, yscale='log', xscale='log')
-plt.plot(h_list, error_forward, 'r')
-plt.plot(h_list, abs(fprime(1/3) - F.forward(1/3, h_list[-1])) * h_list, 'r--')
-plt.plot(h_list, error_central, 'g')
-plt.plot(h_list, abs(fprime(1/3) - F.central(1/3, h_list[-1])) * h_list**2, 'g--')
-plt.plot(h_list, error_extrapolate, 'b')
-plt.plot(h_list, abs(fprime(1/3) - F.extrapolate(1/3, h_list[-1])) * h_list**4, 'b--')
+print __doc__
+
+fig = plt.figure(figsize=(10, 8))
+ax = fig.add_subplot(111, yscale='log', xscale='log')
+ax.plot(h_list, error_forward, 'r', label='Vorwaertsdifferenz')
+ax.plot(h_list, abs(fprime(1/3) - forward(f, 1/3, h_list[-1])) * h_list, 'r--', label='erwartetes Verhalten: $O(h)$')
+ax.plot(h_list, error_central, 'g', label='Zentraldifferenz')
+ax.plot(h_list, abs(fprime(1/3) - central(f, 1/3, h_list[-1])) * h_list**2, 'g--', label='erwartetes Verhalten: $O(h^2)$')
+ax.plot(h_list, error_extrapolate, 'b', label='Extrapolierte Differenz')
+ax.plot(h_list, abs(fprime(1/3) - extrapolate(f, 1/3, h_list[-1])) * h_list**4, 'b--', label='erwartetes Verhalten: $O(h^4)$')
+ax.set_xlabel(r'$h$')
+ax.set_ylabel('relativer Fehler')
+ax.legend(loc='best')
 plt.show()
