a08d891de79544cf36fcebb7629880ddcbb55dd5
BustyBasti <sebastian.k.schmidt@gmx.de>
2015-05-04 19:39:27 +0200

    working version of 3_1

--- Aufgabe 3/Numerical Integration.py
+++ Aufgabe 3/Numerical Integration.py
@@ -3,34 +3,55 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def f1(x):
-    return x
+def fa(x):
+    return np.sin(2*x)
+Int_a = -0.25
 
 
-def F1(x):
-    return 1/2 * x**2
+def fb(x):
+    return np.exp(-100 * x**2)
+Int_b = 0.177245
+
+def fc(x):
+    return (1.0 + np.sign(x))/2
+Int_c = np.pi/3
+
+def middle(func, a, b, n):
+    width = float((b - a)/n)
+    x_i = np.linspace(a, b, n, endpoint=False)
+    f_i = func(x_i + width/2)
+    return sum(f_i) * width
 
 
-class Function(object):
-    def __init__(self, function):
-        self.defn = function
-
-    def middle(self, a, b, n):
-        width = (a - b)/n
-        x_i = np.linspace(a, b, n, endpoint=False)
-        f_i = self.defn(x_i + width/2)
-        return sum(f_i)*width
-
-    def trapez(self, a, b, n):
-        width = (a - b)/n
-        return
-
-    def simpson(self, a, b, n):
-        return n
+def trapez(func, a, b, n):
+    width = float(b - a)/n
+    x_i = np.linspace(a, b, n + 1)
+    int_i = func(x_i[:-1]) + func(x_i[1:])
+    return sum(int_i) * width/2
 
 
-Int1 = Function(f1)
+def simpson(func, a, b, n):
+    return 4/3 * trapez(func, a, b, 2*n) - 1/3 * trapez(func, a, b, n)
 
-print np.linspace(0, 10, 10)
-print np.linspace(0, 10, 10, endpoint=False)
-print np.linspace(0, 10, 11)
+A = -np.pi/2
+B = np.pi/3
+number = 1000
+N_list = np.int32(np.logspace(0, 5, number))
+h_list = (B - A)/N_list
+error_middle = np.zeros(number)
+error_trapez = np.zeros(number)
+error_simpson = np.zeros(number)
+
+for z in range(number):
+    error_middle[z] = abs((middle(fa, A, B, N_list[z]) - Int_a) / Int_a)
+    error_trapez[z] = abs((trapez(fa, A, B, N_list[z]) - Int_a) / Int_a)
+    error_simpson[z] = abs((simpson(fa, A, B, N_list[z]) - Int_a) / Int_a)
+
+
+fig = plt.figure(figsize=(14, 8))
+ax = fig.add_subplot(111, xscale='log', yscale='log')
+ax.plot(h_list, error_middle, 'g.', label='Mittelwertmethode')
+ax.plot(h_list, error_trapez, 'b.', label='Trapezmethode')
+ax.plot(h_list, error_simpson, 'r.', label='Simpson-Methode')
+plt.legend(loc='best')
+plt.show()
